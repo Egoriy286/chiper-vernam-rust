2c72f63df6a91143d9c75385bdc1a955881ee3d1
Monastyrev Egoriy <98502409+Egoriy286@users.noreply.github.com>
2023-11-17 17:55:11 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,21 @@ fn main() {
 
     // Copy contents of file to a mutable string
     data_file.read_to_string(&mut file_content).unwrap();
-    
-    let text = file_content.to_string();
-    let mut key = [0, 0, 0, 0, 0, 0, 0, 0];
-    //let mut encode:String="";
-    for i in 0..text.len(){
-        key[i]=rand::thread_rng().gen_range(0,255);
-        let num = u32::from(text.chars().nth(i).unwrap());
-        //encode.push();
+    // Create an empty mutable Vec
+    let mut key = Vec::new();
+
+    for i in 0..file_content.len(){
+        key.push(rand::thread_rng().gen_range(0,255));
+        let num = u32::from(file_content.chars().nth(i).unwrap());
         let c= char::from_u32(num^key[i]);
-        println!("File content: {} => {:?}", num^key[i], c);
+        println!("Encode: {} => {:?}", num^key[i], c);
     }
     print!("Key=");
-    for i in 0..text.len(){
+    for i in 0..file_content.len(){
         print!("{:?}",char::from_u32(key[i]));
     }
-    
 }
 /*
-use std::fs::File;
-use std::io::Read;
-use rand::Rng;
 fn main() {
     // Read a file in the local file system
     let mut data_file = File::open("data.txt").unwrap();
@@ -42,12 +36,11 @@ fn main() {
     data_file.read_to_string(&mut file_content).unwrap();
     
     let text = file_content.to_string();
-    let mut key = [0, 0, 0, 0, 0, 0, 0, 0];
+    let mut key = Vec::new();
     //let mut encode:String="";
     for i in 0..text.len(){
-        key[i]=rand::thread_rng().gen_range(0,255);
+        key.push(rand::thread_rng().gen_range(0,255));
         let num = u32::from(text.chars().nth(i).unwrap());
-        //encode.push();
         let c= char::from_u32(num^key[i]);
         println!("File content: {} => {:?}", num^key[i], c);
     }
@@ -56,10 +49,5 @@ fn main() {
         print!("{:?}",char::from_u32(key[i]));
     }
     
-}
-
-fn xor(s: Vec<u8>, key: &[u8]) -> Vec<u8> {
-    let mut b = key.iter().cycle();
-    s.into_iter().map(|x| x ^ b.next().unwrap()).collect()
 }
 */
